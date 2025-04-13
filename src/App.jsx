c78a86ef8c754a1ddffc9f09acc14c01b2db3e7f
tracyn09 <tracyn0099@gmail.com>
2025-04-13 19:15:46 -0400

    Handle loading and error states gracefully

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import './styles/styles.css'
 
 function App() { 
   const [tours, setTours] = useState([]) //Hold list of tours fetched from the API
+  const [loading, setLoading] = useState(true) //Loading status
+  const [error, setError] = useState(false) //Error status
 
   const removeTour = (id) => { 
     setTours(tours.filter((tour) => tour.id !== id))
@@ -12,7 +14,7 @@ function App() {
   return (
     <main>
       <h1>Available Tours</h1>
-      <Gallery tours={tours} />
+      <Gallery tours={tours} loading={loading} error={error} removeTour={removeTour} />
       </main>
     );
   }
